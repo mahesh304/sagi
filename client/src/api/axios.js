d3e2fe6d367dd90a568897c12aa2c50fe7bd16cf
Mahesh Velishala <110409062+mahesh304@users.noreply.github.com>
2025-08-16 23:11:55 +0530

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000',
-	withCredentials: false,
+	baseURL: "https://sagifriedchicken.vercel.app",
+	withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
